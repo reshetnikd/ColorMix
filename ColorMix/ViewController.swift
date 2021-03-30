5c9f3c5a9638c1237a6f6c45d63c6a8047cdb20a
reshetnikd <dmitry.reshetnik@icloud.com>
2021-03-30 11:08:47 +0300

    Add updateColor()

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -9,19 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        colorView.backgroundColor = .black
+        updateColor()
     }
 
     @IBAction func switchChanged(_ sender: UISwitch) {
-        if sender.isOn {
-            colorView.backgroundColor = .red
-        } else {
-            colorView.backgroundColor = .black
+        updateColor()
+    }
+    
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn {
+            red = 1
         }
+        
+        if greenSwitch.isOn {
+            green = 1
+        }
+        
+        if blueSwitch.isOn {
+            blue = 1
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
     }
     
 }
