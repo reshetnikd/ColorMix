65c9248749b92fc50f334fbca7c801ed34018989
reshetnikd <dmitry.reshetnik@icloud.com>
2021-03-30 10:20:49 +0300

    Add outlets

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        colorView.backgroundColor = .black
     }
 
 
